529f2f897a94df58a98816cd22073efd50106722
Marek Bartoš <bartos.developer152@gmail.com>
2021-08-19 20:27:47 +0200

    Update orisai/nette-di

--- tests/Unit/Tracy/LazyTracyToPsrLoggerTest.php
+++ tests/Unit/Tracy/LazyTracyToPsrLoggerTest.php
@@ -3,7 +3,6 @@
 namespace Tests\OriNette\Monolog\Unit\Tracy;
 
 use OriNette\DI\Boot\ManualConfigurator;
-use OriNette\DI\Services\MissingService;
 use OriNette\Monolog\Tracy\LazyTracyToPsrLogger;
 use PHPUnit\Framework\TestCase;
 use Psr\Log\Test\TestLogger;
@@ -52,28 +51,6 @@ final class LazyTracyToPsrLoggerTest extends TestCase
 			$logger1->records,
 		);
 		self::assertSame($logger1->records, $logger2->records);
-	}
-
-	public function testInvalidType(): void
-	{
-		$configurator = new ManualConfigurator(dirname(__DIR__, 3));
-		$configurator->setDebugMode(true);
-		$configurator->addConfig(__DIR__ . '/logger.invalid.neon');
-
-		$container = $configurator->createContainer();
-
-		$logger = $container->getByType(LazyTracyToPsrLogger::class);
-
-		$this->expectException(MissingService::class);
-		$this->expectExceptionMessage(<<<'MSG'
-Context: Service logger.one returns instance of stdClass.
-Problem: OriNette\Monolog\Tracy\LazyTracyToPsrLogger supports only instances of
-         Psr\Log\LoggerInterface.
-Solution: Remove service from LazyTracyToPsrLogger or make the service return
-          supported object type.
-MSG);
-
-		$logger->log('test');
 	}
 
 }

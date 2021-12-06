f68531d02e240771684a9fe7dc21180335029c48
Marek Bartoš <bartos.developer152@gmail.com>
2021-12-06 20:42:33 +0100

    Tests: fix PHP version check

--- tests/Unit/DI/MonologExtensionTest.php
+++ tests/Unit/DI/MonologExtensionTest.php
@@ -42,7 +42,7 @@ final class MonologExtensionTest extends TestCase
 		parent::setUp();
 
 		$this->rootDir = dirname(__DIR__, 3);
-		if (PHP_VERSION_ID < 81_000) {
+		if (PHP_VERSION_ID < 8_01_00) {
 			@mkdir("$this->rootDir/var/build");
 		}
 	}
